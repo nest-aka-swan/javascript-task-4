cbf99a75c2788a42aa8851440501363d607a7ed7
Nikita Lebedev <nest-aka-swan@yandex.ru>
2016-10-31 18:23:04 +0500

    initial commit

--- lego.js
+++ lego.js
@@ -6,6 +6,31 @@
  */
 exports.isStar = true;
 
+function isPropertyValid(property) {
+    var validProperties = ['name', 'age', 'gender', 'email', 'phone', 'favoriteFruit'];
+
+    return validProperties.includes(property);
+}
+
+function intersect(a, b) {
+    var i;
+    var helperObj = {};
+    for (i = 0; i < b.length; i++) {
+        helperObj[b[i]] = true;
+    }
+
+    var result = [];
+    var value;
+    for (i = 0; i < a.length; i++) {
+        value = a[i];
+        if (value in helperObj) {
+            result.push(value);
+        }
+    }
+
+    return result;
+}
+
 /**
  * Запрос к коллекции
  * @param {Array} collection
@@ -13,58 +38,149 @@ exports.isStar = true;
  * @returns {Array}
  */
 exports.query = function (collection) {
-    return collection;
+    var queries = [].slice.call(arguments, 1);
+    var result = collection.map(function (entry) {
+        return Object.assign({}, entry);
+    });
+
+    if (collection.length === 0) {
+        return result;
+    }
+
+    var select;
+    var limit = collection.length;
+    var format = {};
+    var queryResult;
+    queries.forEach(function (query) {
+        queryResult = query(result);
+
+        if (queryResult.select && queryResult.select.length !== 0) {
+            if (select) {
+                select = intersect(select, queryResult.select);
+            } else {
+                select = queryResult.select;
+            }
+        }
+
+        if (queryResult.limit) {
+            limit = queryResult.limit;
+        }
+
+        if (queryResult.format) {
+            format[queryResult.format.property] = queryResult.format.formatter;
+        }
+
+        result = queryResult.entries;
+    });
+
+    if (select) {
+        result.forEach(function (entry) {
+            for (var key in entry) {
+                if (!select.includes(key)) {
+                    delete entry[key];
+                }
+            }
+        });
+    }
+
+    result.splice(limit);
+
+    var fmtKeys = Object.keys(format);
+    fmtKeys.forEach(function (key) {
+        result.forEach(function (entry) {
+            entry[key] = format[key](entry[key]);
+        });
+    });
+
+    return result;
 };
 
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {Function}
  */
 exports.select = function () {
-    return;
+    var selected = [].slice.call(arguments);
+
+    return function (entries) {
+        return {
+            select: selected.filter(function (prop) {
+                return isPropertyValid(prop);
+            }),
+            entries: entries
+        };
+    };
 };
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {Function}
  */
 exports.filterIn = function (property, values) {
-    console.info(property, values);
+    return function (entries) {
+        for (var i = entries.length - 1; i >= 0; i--) {
+            if (!values.includes(entries[i][property])) {
+                entries.splice(i, 1);
+            }
+        }
 
-    return;
+        return {
+            entries: entries
+        };
+    };
 };
 
 /**
  * Сортировка коллекции по полю
  * @param {String} property – Свойство для фильтрации
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
+ * @returns {Function}
  */
 exports.sortBy = function (property, order) {
-    console.info(property, order);
+    return function (entries) {
+        entries.sort(function (a, b) {
+            return order === 'asc' ? a[property] > b[property] : a[property] < b[property];
+        });
 
-    return;
+        return {
+            entries: entries
+        };
+    };
 };
 
 /**
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {Function}
  */
 exports.format = function (property, formatter) {
-    console.info(property, formatter);
-
-    return;
+    return function (entries) {
+        return {
+            format: {
+                property: property,
+                formatter: formatter
+            },
+            entries: entries
+        };
+    };
 };
 
 /**
  * Ограничение количества элементов в коллекции
  * @param {Number} count – Максимальное количество элементов
+ * @returns {Function}
  */
 exports.limit = function (count) {
-    console.info(count);
-
-    return;
+    return function limit(entries) {
+        return {
+            limit: count,
+            entries: entries
+        };
+    };
 };
 
 if (exports.isStar) {
@@ -73,17 +189,44 @@ if (exports.isStar) {
      * Фильтрация, объединяющая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Function}
      */
     exports.or = function () {
-        return;
+        var queries = [].slice.call(arguments);
+
+        return function (entries) {
+            var result = [];
+            var copy = Object.assign([], entries);
+
+            queries.forEach(function (query) {
+                entries = Object.assign([], copy);
+
+                result = result.concat(query(entries).entries);
+            });
+
+            return {
+                entries: result
+            };
+        };
     };
 
     /**
      * Фильтрация, пересекающая фильтрующие функции
      * @star
      * @params {...Function} – Фильтрующие функции
+     * @returns {Function}
      */
     exports.and = function () {
-        return;
+        var queries = [].slice.call(arguments);
+
+        return function (entries) {
+            var result = {};
+
+            queries.forEach(function (query) {
+                result.entries = query(entries).entries;
+            });
+
+            return result;
+        };
     };
 }
